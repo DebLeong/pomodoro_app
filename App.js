3dfd0dfa3fbcbaa36db7f9326a62f1a6addadc76
DebLeong <debleon9@gmail.com>
2018-04-06 11:26:49 +0800

    finishing up

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Timer from './components/timer';
+import reducer from './reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+let store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Provider store={store}>
+            <Timer />
+        </Provider>
     );
   }
 }
